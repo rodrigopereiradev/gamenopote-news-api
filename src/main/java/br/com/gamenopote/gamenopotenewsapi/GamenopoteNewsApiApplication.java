b1cf39f0ab47116e3a9406a12a366671ada1daee
Rodrigo Pereira <rodrigopereira.dev@gmai.com>
2018-06-16 17:20:57 -0300

    commit inicial do projeto

---
+++ src/main/java/br/com/gamenopote/gamenopotenewsapi/GamenopoteNewsApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.gamenopote.gamenopotenewsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamenopoteNewsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamenopoteNewsApiApplication.class, args);
+	}
+}
